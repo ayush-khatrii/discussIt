f0ad4edb24b73123678dee4a17ca0e06455852cf
Ayush Khatri <khatriayush2003@gmail.com>
2024-08-28 01:13:48 +0530

    Update SingleGroupInfo.jsx

--- client/src/components/groups/SingleGroupInfo.jsx
+++ client/src/components/groups/SingleGroupInfo.jsx
@@ -6,27 +6,27 @@ import { Link } from 'react-router-dom'
 
 const users = [
   {
-    fullName: "Gautam Ghanshani",
-    username: "gautam",
-    fallback: "gg",
+    fullName: "aline",
+    username: "aline",
+    fallback: "ali",
     isGroupAdmin: true
   },
   {
-    fullName: "Gaurav Lakhwani",
-    username: "gaurav",
-    fallback: "gl",
+    fullName: "anime",
+    username: "anime",
+    fallback: "an",
     isGroupAdmin: false
   },
   {
-    fullName: "Rahul Lakhwani",
-    username: "rahul",
-    fallback: "rl",
+    fullName: "alex",
+    username: "alex",
+    fallback: "al",
     isGroupAdmin: false
   },
   {
-    fullName: "Hem Khatri",
-    username: "hem.khatri",
-    fallback: "hk",
+    fullName: "eliza",
+    username: "eliza",
+    fallback: "el",
     isGroupAdmin: false
   },
 ]
@@ -34,70 +34,65 @@ const users = [
 const SingleGroupInfo = () => {
   return (
     <div>
-      <>
-        <div className='absolute top-5 right-5'>
-          <Sidebar />
-        </div>
-        <div className="max-w-xl mx-auto py-8">
-          <Container className="px-2 mx-auto">
-            <div className="p-6">
-              <div className="flex justify-center items-center mb-10">
-                <div className="flex  justify-center items-center text-center flex-col">
-                  <div className="mb-2">
-                    <Avatar
-                      radius="full"
-                      fallback="Gr"
-                      size="7"
-                    />
-                  </div>
-                  <h1 className='text-3xl capitalize'>Bodmoshiyaan</h1>
-                  <p className='text-zinc-600 my-1 font-medium text-lg'>Group · 4  members</p>
+      <div className="max-w-xl mx-auto py-8">
+        <Container className="px-2 mx-auto">
+          <div className="p-6">
+            <div className="flex justify-center items-center mb-10">
+              <div className="flex  justify-center items-center text-center flex-col">
+                <div className="mb-2">
+                  <Avatar
+                    radius="full"
+                    fallback="Gr"
+                    size="7"
+                  />
                 </div>
-              </div>
-              <div className='text-center'>
-                <p className='text-zinc-600 font-medium text-xl'>
-                  About
-                </p>
-                <p className='text-xl w-full'>Groups OF Bakwaas lorem</p>
+                <h1 className='text-3xl capitalize'>Bodmoshiyaan</h1>
+                <p className='text-zinc-600 my-1 font-medium text-lg'>Group · 4  members</p>
               </div>
             </div>
-            <div className='p-5 flex flex-col justify-start'>
-              <p className='text-center my-5 text-zinc-500 font-medium text-xl p-5 '>Group members</p>
-              {users?.map((user, index) => (
-                <Link key={index} to={`/chats/${user.username}`}>
-                  <div className='p-3'>
-                    <Flex gap="3" justify="between" align="center">
-                      <Flex gap="3" justify="center" align="center">
-                        <div>
-                          <Avatar
-                            radius="full"
-                            size="5"
-                            fallback={user.fallback}
-                          />
-                        </div>
-                        <Flex justify="" align="" gap="9">
-                          <h1 className='text-xl sm:text-xl  text-zinc-200'>
-                            {user.fullName}
-                          </h1>
-                          {user.isGroupAdmin &&
-                            <span>
-                              <Badge color="gray" variant="surface" highContrast>Admin</Badge>
-                            </span>
-                          }
-                        </Flex>
+            <div className='text-center'>
+              <p className='text-zinc-600 font-medium text-xl'>
+                About
+              </p>
+              <p className='text-xl w-full'>Groups OF Bakwaas lorem</p>
+            </div>
+          </div>
+          <div className='p-5 flex flex-col justify-start'>
+            <p className='text-center my-5 text-zinc-500 font-medium text-xl p-5 '>Group members</p>
+            {users?.map((user, index) => (
+              <Link key={index} to={`/chats/${user.username}`}>
+                <div className='p-3'>
+                  <Flex gap="3" justify="between" align="center">
+                    <Flex gap="3" justify="center" align="center">
+                      <div>
+                        <Avatar
+                          radius="full"
+                          size="5"
+                          fallback={user.fallback}
+                        />
+                      </div>
+                      <Flex justify="" align="" gap="9">
+                        <h1 className='text-xl sm:text-xl  text-zinc-200'>
+                          {user.fullName}
+                        </h1>
+                        {user.isGroupAdmin &&
+                          <span>
+                            <Badge color="gray" variant="surface" highContrast>Admin</Badge>
+                          </span>
+                        }
                       </Flex>
                     </Flex>
-                  </div>
-                </Link>
-              ))
-              }
-            </div>
-            <Flex align="center" justify="center" p="5">
-              <SlCalender /><p className='px-3 opacity-50'>Created on 1-02-2000</p>
-            </Flex>
-          </Container>
-        </div>
-      </>
+                  </Flex>
+                </div>
+              </Link>
+            ))
+            }
+          </div>
+          <Flex align="center" justify="center" p="5">
+            <SlCalender /><p className='px-3 opacity-50'>Created on 1-02-2000</p>
+          </Flex>
+        </Container>
+      </div>
     </div>
   )
 }
