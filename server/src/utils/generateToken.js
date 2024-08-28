a812cb7137417346d4220ec08d66fbb9e0b27acb
Ayush Khatri <108052277+ayush-khatrii@users.noreply.github.com>
2024-08-28 16:44:06 +0530

    Update generateToken.js

--- server/src/utils/generateToken.js
+++ server/src/utils/generateToken.js
@@ -10,7 +10,7 @@ export const generateToken = (userId, res) => {
     httpOnly: true,
     sameSite: "strict",
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    secure: process.env.NODE_ENV !== "development",
+    secure: true,
   });
 };
 export const verifyToken = (user, token) => {
